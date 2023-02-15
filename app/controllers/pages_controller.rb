2f3fb902011dd5e16df80a44cfbfe4555948ed2c
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-15 18:19:31 +0000

    Routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,7 @@ if devise_api_token
 render json: { message: "You are logged in as #{devise_api_token.resource_owner.email}" }, status: :ok
 else
 render json: { message: 'You are not logged in' }, status: :unauthorized
-end
-end
+end 
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     scope '/v1' do 
       get 'pages/home', to: 'pages#home'
       get 'pages/restricted', to: 'pages#restricted'
-      devise_for :users 
+      devise_for :users
     end
   end 
 end 
